0559a0c7fe504c8aea237ad0f58863c0ffa6b14d
reaganodhiambo <reaganodhiambo003@gmail.com>
2022-04-07 10:59:32 +0300

    first commit

---
+++ Dev/base/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('', views.home, name="home"),
+    path('rooms/<str:pk>/', views.room, name="rooms"),
+    
+    path('create-room/', views.createRoom, name="create-room"),
+    path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
+    path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
+]
+
